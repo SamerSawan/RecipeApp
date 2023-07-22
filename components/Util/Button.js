798dc01657c7e0714b0980c5e6b12846e85fe4c0
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2023-07-21 20:43:26 -0400

    preparing filter button to accept a filter screen

--- components/Util/Button.js
+++ components/Util/Button.js
@@ -1,6 +1,6 @@
 import { Pressable, Text } from 'react-native'
 
-function Button({ children, style }){
+function Button({ children, style, onPress }){
     return (
         <Pressable>
             <Text style={style}>{children}</Text>
