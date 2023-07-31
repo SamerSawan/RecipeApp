2ec260d824457c5a78a9cbae4f12c071fa42c36a
Samer Sawan <samersawan123@gmail.com>
2023-07-31 17:28:43 -0400

    fixed the way things are arranged inside of the add recipe screen

--- components/Forms/AddRecipeForm.js
+++ components/Forms/AddRecipeForm.js
@@ -1,107 +1,118 @@
-import { useState } from 'react';
-import { View, Image } from "react-native"
-import * as ImagePicker from 'expo-image-picker';
-import Input from "./Input"
-import ListInput from "./ListInput"
-import Button from '../Util/Button';
+import { useState } from "react";
+import { View, Image, Pressable } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import Input from "./Input";
+import ListInput from "./ListInput";
+import Button from "../Util/Button";
+import Ingredient from "../../models/ingredient";
 
-import Ingredient from '../../models/ingredient';
+function AddRecipeForm() {
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [image, setImage] = useState(null);
+  const [ingredients, setIngredients] = useState([]);
+  const [ingredient, setIngredient] = useState(null);
+  const [quantity, setQuantity] = useState("");
+  const [instructions, setInstructions] = useState([]);
+  const [instruction, setInstruction] = useState("");
 
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
-
-
-function AddRecipeForm(){
-    const [name, setName] = useState('');
-    const [desc, setDesc] = useState('');
-    const [image, setImage] = useState(null);
-    const [ingredients, setIngredients] = useState([])
-    const [ingredient, setIngredient] = useState(null)
-    const [quantity, setQuantity] = useState('')
-    const [instructions, setInstructions] = useState([])
-    const [instruction, setInstruction] = useState('')
-
-
-    const pickImage = async () => {
-        // No permissions request is necessary for launching the image library
-        let result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.All,
-          allowsEditing: true,
-          aspect: [4, 3],
-          quality: 1,
-        });
-
-        if (!result.canceled) {
-        setImage(result.assets[0].uri);
-        }
-    };
-
-    function nameChangeHandler(enteredText) {
-        setName(enteredText);
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
     }
+  };
 
-    function descChangeHandler(enteredText) {
-        setDesc(enteredText);
-    }
+  function nameChangeHandler(enteredText) {
+    setName(enteredText);
+  }
 
-    function ingredientChangeHandler(enteredText) {
-        setIngredient(enteredText);
-    }
+  function descChangeHandler(enteredText) {
+    setDesc(enteredText);
+  }
 
-    function quantityChangeHandler(enteredText) {
-        setQuantity(enteredText);
-    }
+  function ingredientChangeHandler(enteredText) {
+    setIngredient(enteredText);
+  }
 
-    function instructionChangeHandler(enteredText){
-        setInstruction(enteredText);
-    }
+  function quantityChangeHandler(enteredText) {
+    setQuantity(enteredText);
+  }
 
-    return (
-        <View className="">
-            <View className="my-10 items-center">
-                <Input 
-                label="Name" 
-                placeholder={"Recipe Name"}
-                onChangeText={nameChangeHandler}
-                value={name} 
-                style="flex-row w-40"/>
+  function instructionChangeHandler(enteredText) {
+    setInstruction(enteredText);
+  }
+
+  function addIngredient() {}
+
+  return (
+    <View className="">
+      <View className="flex-row border-2 pt-2 mt-10 mb-5 w-96">
+        <View>
+          <Input
+            label="Name"
+            placeholder={"Recipe Name"}
+            onChangeText={nameChangeHandler}
+            value={name}
+            style="w-40"
+          />
+          <View className="mb-10 items-center">
+            <View className="w-36">
+              {image ? (
+                <Pressable className="items-center mt-2" onPress={pickImage}>
+                  <Image
+                    className="rounded-lg"
+                    source={{ uri: image }}
+                    style={{ width: 100, height: 100 }}
+                  />
+                </Pressable>
+              ) : (
+                <Button onPress={pickImage}>Pick Image</Button>
+              )}
             </View>
-            <View className="mb-10">
-                <Input 
-                label="Description" 
-                placeholder={"Description"}
-                onChangeText={descChangeHandler}
-                value={desc} />
-            </View>
-            <View className="mb-10 items-center">
-                <View className="w-36">
-                    <Button onPress={pickImage}>Pick Image</Button>
-                    {image && <Image className="rounded-lg" source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-                </View>
-            </View>
-            <View>
-                <View>
-                    <ListInput 
-                    label="Ingredients" 
-                    placeholder="Ingredient Name" 
-                    onChangeText={ingredientChangeHandler}
-                    value={ingredient}
-                    quantityOnChangeText={quantityChangeHandler}
-                    quantity={quantity}
-                    isIngredient={true} 
-                    buttonTitle="add ingredient" />
-                </View>
-                <View className="mt-4">
-                    <ListInput 
-                    label="Instructions" 
-                    placeholder="Instruction" 
-                    buttonTitle="add instruction" 
-                    onChangeText={instructionChangeHandler}
-                    value={instruction}
-                    />
-                </View>
-            </View>
+          </View>
         </View>
-    )
+        <View className="">
+          <Input
+            label="Description"
+            placeholder={"Description"}
+            onChangeText={descChangeHandler}
+            value={desc}
+            style="w-64 h-32"
+          />
+        </View>
+      </View>
+      <View>
+        <View className="border-2 items-center">
+          <ListInput
+            label="Ingredients"
+            placeholder="Ingredient Name"
+            onChangeText={ingredientChangeHandler}
+            value={ingredient}
+            quantityOnChangeText={quantityChangeHandler}
+            quantity={quantity}
+            isIngredient={true}
+            buttonTitle="add ingredient"
+          />
+        </View>
+        <View className="mt-4">
+          <ListInput
+            label="Instructions"
+            placeholder="Instruction"
+            buttonTitle="add instruction"
+            onChangeText={instructionChangeHandler}
+            value={instruction}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
-export default AddRecipeForm
+export default AddRecipeForm;
--- components/Forms/Input.js
+++ components/Forms/Input.js
@@ -3,12 +3,13 @@ import { TextInput, Text, View } from "react-native"
 
 function Input({label, placeholder, onChangeText, value, style}){
     return (
-        <View className={style + " items-center justify-between"}>
+        <View className={"items-center"}>
             <Text className="text-primary200">
                 {label}
             </Text>
-            <TextInput 
-            className="text-black border-2 border-primary150 px-2 py-0.5 rounded-md max-w-2/3"
+            <TextInput
+            
+            className={style + " text-black border-2 border-primary150 px-2 py-0.5 rounded-md max-w-2/3"}
             multiline 
             blurOnSubmit 
             onChangeText={onChangeText}
